9523e30314d401ae7134eec0b2d43f9158d2b454
adeeb5378 <adeeb0671@gmail.com>
2021-12-27 12:53:51 +0530

    Day 2 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,12 +10,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
+    // double value = 30.9;
+    // String name = "CodePur";
+    // bool isMale = true;
+    // num temp = 30.9;
+    // var a = "Adeeb";
+    // const pi = 3.14; // Can't change
+
     return MaterialApp(
-      home: Center(
-        child: Container(
-          child: Text("Welcome to 30 days of Flutter"),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
