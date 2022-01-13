932e21cfeca2e853c61be6a4792783a3961e32d3
adeeb5378 <adeeb0671@gmail.com>
2022-01-13 15:48:36 +0530

    Theme Extracted

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,14 +1,22 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 
 class HomePage extends StatelessWidget {
+
   final int days = 30;
   final String name = "Codepur";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
+        //backgroundColor: Colors.white,
+        //elevation: 0.0,
+        //iconTheme: IconThemeData(color: Colors.black),
+        title: Text(
+          "Catalog App"),
       ),
       body: Center(
         child: Container(
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+// ignore_for_file: prefer_const_constructors, deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/Pages/home_page.dart';
 import 'package:flutter_catalog/Pages/login_page.dart';
 import 'package:flutter_catalog/utills/routes.dart';
+import 'package:flutter_catalog/widgets/Themes.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -13,12 +16,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-        
-      ),
+      themeMode: ThemeMode.light,
+      theme: MyTheme.lightTheme(context) ,
+      darkTheme: MyTheme.dardTheme(context),
       debugShowCheckedModeBanner: false,
+      initialRoute: myRoutes.homeRoute,
       routes: {
         //home: HomePage(), //Same as "/" Route
         "/" : (context) => LoginPage(),
